fb54c80bd61447fe59d050734a75aacd29451d0c
Facundo Ibarra <facundo.ibarra.c@gmail.com>
2022-04-16 16:32:45 -0300

    Se arreglo register para que funcione. Se limitó el acceso a ciertas funciones si no se está logeado

--- miembros/forms.py
+++ miembros/forms.py
@@ -3,13 +3,18 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegistrarseForm(UserCreationForm):
-    email = forms.EmailField()
-    nombre= forms.CharField(max_length=100)
-    apellido = forms.CharField(max_length=100)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    nombre= forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
+    apellido = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = User
         #fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
         fields = UserCreationForm.Meta.fields + ('nombre', 'apellido', 'email')
 
+    def __init__(self, *args, **kwargs):
+        super(RegistrarseForm, self).__init__(*args, **kwargs)
 
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
